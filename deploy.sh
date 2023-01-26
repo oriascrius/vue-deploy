7ae3c02a9390bf1dd451c3a464cbf6c7d88617cd
oriascrius <qoq77416416@gmail.com>
2023-01-26 16:50:24 +0800

    Initial commit

---
+++ deploy.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# place .nojekyll to bypass Jekyll processing
+echo > .nojekyll
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git checkout -B main
+git add -A
+git commit -m 'deploy'
+
+
+
+# 以下兩種擇一開啟
+
+# 部屬在 main 上
+# if you are deploying to https://oriascrius.github.io
+# git push -f git@github.com:oriascrius/oriascrius.github.io.git main
+
+# 部屬在 gh-pages 上
+# if you are deploying to https://oriascrius.github.io/Hex-all_practice
+git push -f git@github.com:oriascrius/Hex-all_practice/vue-vite main:gh-pages
+
+cd -
---
+++ src/views/AboutView.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <h2>{{ data }}</h2>
+  </div>
+</template>
+
+
+
+<script>
+// randomuser 測試 API
+export default {
+  data(){
+    return{
+      data:{}
+    }
+  },
+  mounted(){
+    console.log(import.meta.env.VITE_TEXT)
+    const url = import.meta.env.VITE_PATH;
+    this.$http.get(url)
+    .then((res) =>{
+      console.log(res);
+      this.data = res.data.results[0];
+    });
+  },
+};
+</script>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
